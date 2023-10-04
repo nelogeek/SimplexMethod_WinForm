44951e42a64d54c6439471e23d86a0cc0ac66b3c
nelogeek <lokotaevpr@gmail.com>
2023-10-04 10:48:11 +0500

    Добавьте файлы проекта.

---
+++ SimplexMethod/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SimplexMethod
+{
+    public partial class Form1 : Form
+    {
+        int countVar = 0;
+        int countCondition = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            countVar = Convert.ToInt32(textBox2.Text);
+            countCondition = Convert.ToInt32(textBox1.Text);
+            fillDataGrid();
+        }
+
+        void fillDataGrid()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.ColumnCount = countVar + 1;
+            dataGridView1.RowHeadersVisible = false;
+            for (int i = 0; i < countVar + 1; i++)
+            {
+                if (i < countVar)
+                {
+                    dataGridView1.Columns[i].Name = "x" + (i + 1).ToString();
+                }
+                else if (i == countVar)
+                {
+                    dataGridView1.Columns[i].Name = "b";
+                }
+            }
+
+            for (int i = 0; i < countCondition; i++)
+            {
+                string[] Row = new string[countVar + 1];
+                dataGridView1.Rows.Add(Row);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double[,] Mas = new double[countCondition + 1, countVar + 1];
+            double[,] TrMas;
+            fillMas(Mas);
+            TrMas = TransportMas(Mas);
+            Simplex S = new Simplex(TrMas);
+            double[,] Res = S.Calculate();
+            string mes = "";
+            for (int j = 0; j < countVar + countCondition + 1; j++)
+            {
+                if (j == 0)
+                {
+                    mes = "F = " + Math.Round(-1 * Res[countVar, j], 3) + "\n";
+                }
+                else if (j > countVar)
+                {
+                    mes += "X" + (j - countVar).ToString() + " = " + Math.Round(-1 * Res[countVar, j], 3) + "\n";
+                }
+            }
+            MessageBox.Show(mes);
+        }
+
+        void fillMas(double[,] Mas)
+        {
+            for (int i = 0; i < countCondition + 1; i++)
+            {
+                for (int j = 0; j < countVar + 1; j++)
+                {
+                    if (dataGridView1.Rows[i].Cells[j].Value != null)
+                    {
+                        Mas[i, j] = Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value);
+                    }
+                }
+            }
+        }
+
+        private void CreateDataTable()
+        {
+            // Получаем значения из textBox1 и textBox2
+            int rowCount = Convert.ToInt32(textBox1.Text);
+            int colCount = Convert.ToInt32(textBox2.Text);
+
+            // Очищаем dataGridView1, если она уже содержит данные
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            // Создаем столбцы в dataGridView1
+            for (int col = 0; col < colCount; col++)
+            {
+                dataGridView1.Columns.Add("Column" + col, "Column" + col);
+            }
+
+            // Создаем строки в dataGridView1
+            for (int row = 0; row < rowCount; row++)
+            {
+                dataGridView1.Rows.Add();
+            }
+        }
+
+        double[,] TransportMas(double[,] Mas)
+        {
+            double[,] TrMas = new double[countVar + 1, countCondition + 1];
+            for (int i = 0; i < countVar + 1; i++)
+            {
+                for (int j = 0; j < countCondition + 1; j++)
+                {
+                    TrMas[i, j] = -1 * Mas[j, i];
+                }
+            }
+            return TrMas;
+        }
+    }
+}
+
+
---
+++ SimplexMethod/Simplex.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplexMethod
+{
+    class Simplex
+    {
+        double[,] table;
+
+        int m, n;
+
+        List<int> basis;
+
+        public Simplex(double[,] source)
+        {
+            m = source.GetLength(0);
+            n = source.GetLength(1);
+            table = new double[m, n + m - 1];
+            basis = new List<int>();
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < table.GetLength(1); j++)
+                {
+                    if (j < n)
+                        table[i, j] = source[i, j];
+                    else
+                        table[i, j] = 0;
+                }
+                if ((n + i) < table.GetLength(1))
+                {
+                    table[i, n + i] = 1;
+                    basis.Add(n + i);
+                }
+            }
+
+            n = table.GetLength(1);
+        }
+        public double[,] Calculate()
+        {
+            int mainCol, mainRow;
+
+            while (!IsItEnd())
+            {
+                mainCol = findMainCol();
+                mainRow = findMainRow(mainCol);
+                basis[mainRow] = mainCol;
+
+                double[,] new_table = new double[m, n];
+
+                for (int j = 0; j < n; j++)
+                    new_table[mainRow, j] = table[mainRow, j] / table[mainRow, mainCol];
+
+                for (int i = 0; i < m; i++)
+                {
+                    if (i == mainRow)
+                        continue;
+
+                    for (int j = 0; j < n; j++)
+                        new_table[i, j] = table[i, j] - table[i, mainCol] * new_table[mainRow, j];
+                }
+                table = new_table;
+            }
+            return table;
+        }
+
+        private bool IsItEnd()
+        {
+            bool flag = true;
+
+            for (int j = 1; j < n; j++)
+            {
+                if (table[m - 1, j] > 0)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+
+            return flag;
+        }
+
+        private int findMainCol()
+        {
+            int mainCol = 1;
+
+            for (int j = 2; j < n; j++)
+                if (table[m - 1, j] > table[m - 1, mainCol])
+                    mainCol = j;
+
+            return mainCol;
+        }
+
+        private int findMainRow(int mainCol)
+        {
+            int mainRow = 0;
+
+            for (int i = 0; i < m - 1; i++)
+                if (table[i, mainCol] > 0)
+                {
+                    mainRow = i;
+                    break;
+                }
+
+            for (int i = mainRow + 1; i < m - 1; i++)
+                if ((table[i, mainCol] > 0) && ((table[i, 0] / table[i, mainCol]) < (table[mainRow, 0] / table[mainRow, mainCol])))
+                    mainRow = i;
+
+            return mainRow;
+        }
+    }
+}
